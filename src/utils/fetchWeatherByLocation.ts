a524896a606a4dae2cd8066f482dfa349a1aaae5
Muslim0915 <muslimpro0915@gmail.com>
2024-05-19 13:35:52 +0500

    initial commit

--- src/utils/fetchWeatherByLocation.ts
+++ src/utils/fetchWeatherByLocation.ts
@@ -5,7 +5,7 @@ const toast = useToast();
 export const fetchWeatherByLocation = async (location: string) => {
     try {
         const API_URL = import.meta.env.VITE_APP_API_URL as string;
-        const response = await fetch(API_URL.replace('{{location}}', location));
+        const response = await fetch(API_URL?.replace('{{location}}', location));
         useWeatherStore().state.weathers = await response.json();
         if (!response.ok) {
             toast.error('Please enter a valid location');
